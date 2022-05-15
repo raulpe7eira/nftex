049b62a6942499cb5e24c45bc6021367b29af5ed
Raul Pereira <raul.pereira@gmail.com>
2022-05-14 21:09:26 -0300

    config graphql

--- lib/nftex_web/router.ex
+++ lib/nftex_web/router.ex
@@ -5,6 +5,13 @@ defmodule NftexWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api" do
+    pipe_through :api
+
+    forward "/graphql", Absinthe.Plug, schenma: NftexWeb.Schema
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schenma: NftexWeb.Schema
+  end
+
   scope "/api", NftexWeb do
     pipe_through :api
   end
